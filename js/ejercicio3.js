2fa4b465856b1a2978d0d25a22b9d775c4734e46
unknown <jppinzon26@misena.edu.co>
2022-08-16 12:23:52 -0500

    ejercicio3

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,5 +1,5 @@
-let onces=['arepa' , 'pan' , 'tinto' , 'jugo' , 'empanada'];
-//onces[1];
+/*let onces=['arepa' , 'pan' , 'tinto' , 'jugo' , 'empanada'];
+onces[1];
 onces.push('gaseosa')
 console.long(onces)
 onces.forEach(function(elemento, index, array){
@@ -7,4 +7,13 @@ onces.forEach(function(elemento, index, array){
 }
 )
 onces.length;
-let ultimo= onces.length -1;
+let ultimo= onces.length -1;*/
+
+let aprendices =[
+    {primernombre:"Yhelsin", primerapellido:"Alvarado"},
+    {primernombre:"Jennifer", primerapellido:"Fajardo"}
+]
+
+let nombrecompleto= aprendices.map(function(el, in, or){
+    return'${el.primernombre} ${el.primerapellido}'
+})
